86f203b92732e424cfe3129bc5057a58f78b5d69
Darian Brooks <brooksd@alumni.stanford.edu>
2023-09-08 13:25:43 -0700

    pin table name column updated to hit constraints

--- app/seeds/pins.py
+++ app/seeds/pins.py
@@ -1,17 +1,18 @@
 from app.models import db, Pin, environment, SCHEMA
 from sqlalchemy.sql import text
 from faker import Faker
-from faker.providers import lorem, date_time
+from faker.providers import lorem, date_time, person
 import random
 
 fake = Faker()
 fake.add_provider(date_time)
 fake.add_provider(lorem)
+fake.add_provider(person)
 
 def create_pins(num_pins, num_users, num_boards):
     for _ in range(num_pins):
         yield Pin(
-            name = fake.sentence(nb_words=10, variable_nb_words=False),
+            name = fake.name(),
             description = fake.paragraph(nb_sentences=5),
             url = f'https://picsum.photos/250.jpg?random={random.randint(1,100)}',
             creatorId = random.randint(1, num_users),
