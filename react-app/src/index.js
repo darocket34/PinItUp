5e18cba8331f2b22364263137cbe84f4df017708
Darian Brooks <brooksd@alumni.stanford.edu>
2023-10-04 14:24:04 -0700

    added google analytics

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -7,7 +7,7 @@ import { ModalProvider, Modal } from "./context/Modal";
 import configureStore from "./store";
 import * as sessionActions from "./store/session";
 import App from "./App";
-
+import ReactGA from 'react-ga';
 import "./index.css";
 import PinSearchProvider from "./context/PinSearch";
 
@@ -17,6 +17,9 @@ if (process.env.NODE_ENV !== "production") {
 	window.store = store;
 	window.sessionActions = sessionActions;
 }
+
+const TRACKING_ID = "UA-12341234-1"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 // Wrap the application with the Modal provider and render the Modal component
 // after the App component so that all the Modal content will be layered as
